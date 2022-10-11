d06bb737cc0435cc6445d59018f6fd5aee0c861e
halim <halimzaafane59@gmail.com>
2022-10-11 16:16:25 +0200

    update111022findesession

--- php/site_voitures/index.php
+++ php/site_voitures/index.php
@@ -1,8 +1,8 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=site_voitures;', 'root', '');
-$query = 'SELECT  Marque, Couleur_originale, kilometrage from voitures natural join marques natural join historique_vehicules natural join couleur_garnissage_intérieur';
+$bdd = new PDO('mysql:host=localhost;dbname=site_voitures;','root','');
+$query = 'SELECT  Marque, Couleur_originale, kilometrage, src from voitures natural join marques natural join historique_vehicules natural join couleur_garnissage_intérieur natural join images';
 $allvoitures = $bdd->query($query);
-if(isset($_GET['s']) or isset($_GET['kilometrage']) or isset($_GET['couleur'])) {
+if(isset($_GET['s']) or isset($_GET['kilometrage']) or isset($_GET['couleur']) or isset($_GET['src'])) {
     $recherche = htmlspecialchars($_GET['s']);
     $kilometrage = htmlspecialchars($_GET['kilometrage']);
     $couleur = htmlspecialchars($_GET['couleur']);
@@ -17,7 +17,6 @@ if(!empty($couleur)){
 if(!empty($kilometrage)){
     $condition[] ='Kilometrage like "%'.$kilometrage.'%"';
 }
-
 $sql = $query;
 if(count($condition) > 0){
     $sql .= " WHERE " . implode(' AND ', $condition);
@@ -43,50 +42,65 @@ $allvoitures = $bdd->query($sql);
 
 <body>
     <div class="container1">
+        <div class="input">
     <h1>VROOMISSIMO</h1>
-   <form method="GET" class="formu">
+   <form method="GET" class="formu"><input class="envoyer" type="submit" name="envoyer">
     <i class="fa fa-car" for="s"><span> Rechercher par Marque: </span></i>
     <input type="search" name="s" placeholder=" Marques " autocomplete="off" style="border-radius:10px"><br/><br/>
     <label class="fa fa-car" for="couleur"><span> Filtrer par couleur: </span></label>
     <input type="search" id="couleur" name="couleur" placeholder="Couleurs" style="border-radius:10px"><br/><br/>
     <label class="fa fa-car" for="kilometrage"><span> Filtrer par kilometrage: </span></label>
     <input type="search" id="kilometrage" name="kilometrage" placeholder="Kilométrage" style="border-radius:10px">
-    <input class="envoyer" type="submit" name="envoyer">
+    
    </form> 
+</div>
+   
+   <!-- <table class="table table-succes table-striped" id="jolie">
+<tr> <td id="td"> Marques </td> <td id="td"> Couleurs </td> <td id="td"> Kilométrage </td> <td id="td"> Photos </td> </tr> -->
 
-   <section class="afficher_Voitures">
-   <table class="table table-succes table-striped" id="jolie">
-<tr> <td id="td"> Marques </td> <td id="td"> Couleurs </td> <td id="td"> Kilométrage</td> </tr>
 
-</section>
+<div class="afficher_Voitures">
     <?php
     if($allvoitures->rowCount() > 0){
 while($voitures = $allvoitures->fetch()){
-    ?>
-    <?= "<tr><td>".$voitures['Marque']."</td>
-             <td>".$voitures['Couleur_originale']."</td>
-             <td>".$voitures['kilometrage']."</td></tr><br/>\n"
-             ;
-             
-        ?>
-        
-    <?php
+    //var_dump($voitures)
+    echo <<<html
+    <div class="card alignement "  style="width: 30rem;">  
+    <img src="{$voitures['src']}" class="card-img-top" alt="...">   
+    <div class="card-body">
+    <p> Marque : {$voitures['Marque']}</p>
+    <p> couleur : {$voitures['Couleur_originale']}</p>
+    <p> kilometrage : {$voitures['kilometrage']} kms</p>
+    <a href="#" class="card-link">voir la voiture</a>
+    </div>
+  </div>
+  html; 
+       
+    // cette partie est fonctionnelle
+    // "<tr><td>".$voitures['Marque']."</td>
+    //      <td>".$voitures['Couleur_originale']."</td>
+    //      <td>".."</td>
+    //      <td><img width='300px'src='".$voitures['src']."'></td>
+    //      </tr><br/>\n"
+    //      ;
 
+     ?> 
+    
+    <?php
 }
     }else{
         ?>
-        <p>Aucun Modele trouvé</p>
+        <p id="none">Aucun Modele trouvé</p>
         <?php
     }
     ?>
-    <?php
-echo "<p>Copyright &copy; 1999-" . date("Y") . "  ZAAFANE Halim</p>";
-?>
+  </div>
 </div>
 
+<footer>
+<?php
+echo "<p>Copyright &copy; " . date("Y") . "  ZAAFANE Halim</p>";
+?>
+</footer>
 </body>
-
-
-   
-
 </html>
