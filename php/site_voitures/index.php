bc27261fb71c253d5d1be265a5591412edd38548
halim <halimzaafane59@gmail.com>
2022-10-11 09:15:01 +0200

    update111022

--- php/site_voitures/index.php
+++ php/site_voitures/index.php
@@ -1,10 +1,30 @@
 <?php
 $bdd = new PDO('mysql:host=localhost;dbname=site_voitures;', 'root', '');
-$allusers = $bdd->query('SELECT ID_Voitures, Marque, photos, Modele , Couleur_originale, Couleur_intérieur, Garniture from voitures natural join types natural join marques natural join couleur_garnissage_intérieur order by ID_Voitures ASC');
-if(isset($_GET['s']) AND !empty($_GET['s'])){
+$query = 'SELECT  Marque, Couleur_originale, kilometrage from voitures natural join marques natural join historique_vehicules natural join couleur_garnissage_intérieur';
+$allvoitures = $bdd->query($query);
+if(isset($_GET['s']) or isset($_GET['kilometrage']) or isset($_GET['couleur'])) {
     $recherche = htmlspecialchars($_GET['s']);
-    $allusers = $bdd->query('SELECT ID_Voitures, Marque, photos, Modele , Couleur_originale FROM voitures natural join types natural join marques natural join couleur_garnissage_intérieur WHERE Couleur_originale LIKE "%'.$recherche.'%"');
+    $kilometrage = htmlspecialchars($_GET['kilometrage']);
+    $couleur = htmlspecialchars($_GET['couleur']);
+    $condition = array();
+
+if(!empty($recherche)){
+    $condition[] = 'Marque like "%'.$recherche.'%"';
 }
+if(!empty($couleur)){
+    $condition[] = 'Couleur_originale like "%'.$couleur.'%"';
+}
+if(!empty($kilometrage)){
+    $condition[] ='Kilometrage like "%'.$kilometrage.'%"';
+}
+
+$sql = $query;
+if(count($condition) > 0){
+    $sql .= " WHERE " . implode(' AND ', $condition);
+}
+$allvoitures = $bdd->query($sql);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -13,31 +33,42 @@ if(isset($_GET['s']) AND !empty($_GET['s'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Rechercher des Modeles dans la table voitures</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>VROOMISSIMO</title>
 </head>
-<h1>Exo barre de recherche</h1>
-<h2>Recherche de voitures par couleur</h2>
+
 <body>
-   <form method="GET">
-    <input type="search" name="s" placeholder="Rechercher Voitures par Couleurs">
+    <div class="container1">
+    <h1>VROOMISSIMO</h1>
+   <form method="GET" class="formu">
+    <i class="fa fa-car" for="s"><span> Rechercher par Marque: </span></i>
+    <input type="search" name="s" placeholder=" Marques " autocomplete="off"><br/>
+    <label class="fa fa-car" for="couleur"><span> Filtrer par couleur: </span></label>
+    <input type="search" id="couleur" name="couleurs" placeholder="Couleurs"><br/>
+    <label class="fa fa-car" for="kilometrage"><span> Filtrer par kilometrage: </span></label>
+    <input type="search" id="kilometrage" name="kilometrages" placeholder="Kilométrage">
     <input type="submit" name="envoyer">
    </form> 
 
-
-   <section class="afficher_Modele">
+   <section class="afficher_Voitures">
+   <table class="table table-succes table-striped" id="jolie">
+<tr> <td> Marques </td> <td> Couleurs </td> <td> Kilométrage</td> </tr>
+</section>
    
     <?php
-    if($allusers->rowCount() > 0){
-while($users = $allusers->fetch()){
+    if($allvoitures->rowCount() > 0){
+while($voitures = $allvoitures->fetch()){
     ?>
-   <table class="centre" id="jolie">
-<tr> <td> ID_Voitures </td> <td> Marque </td> <td> Photos </td><td> Modele </td><td> Couleur_originale </td> </tr>
-    <?= "<tr><td>".$users['ID_Voitures']."</td>
-            <td>".$users['Marque']."</td>
-            <td>".$users['photos']."</td>
-            <td>".$users['Modele']."</td>
-            <td>".$users['Couleur_originale']."</td></tr><br/>\n";
+    <?= "<tr><td>".$voitures['Marque']."</td>
+             <td>".$voitures['Couleur_originale']."</td>
+             <td>".$voitures['kilometrage']."</td>
+         </tr><br/>\n"
+             ;
+             
         ?>
     <?php
 }
@@ -47,7 +78,7 @@ while($users = $allusers->fetch()){
         <?php
     }
     ?>
-   </section>
-
+  
+</div>
 </body>
 </html>
