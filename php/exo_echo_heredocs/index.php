efc7a2c60de8117605cfcfb375376b55d4e90998
halim <halimzaafane59@gmail.com>
2022-10-17 16:16:55 +0200

    update171022

---
+++ php/exo_echo_heredocs/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+    $text = <<<fred
+ <table class="table">
+ <thead>
+   <tr>
+     <th scope="col">#</th>
+     <th scope="col">Nom</th>
+     <th scope="col">Prénom</th>
+     <th scope="col">Age</th>
+     <th scope="col">Sexe</th>
+     <th scope="col">Profession</th>
+   </tr>
+ </thead>
+ <tbody>
+   <tr>
+     <th scope="row">1</th>
+     <td>Mark</td>
+     <td>Otto</td>
+     <td>26</td>
+     <td>M</td>
+     <td>Menuisier</td>
+   </tr>
+   <tr>
+     <th scope="row">2</th>
+     <td>Jacob</td>
+     <td>Thornton</td>
+     <td>10</td>
+     <td>M</td>
+     <td>Menuisier</td>
+   </tr>
+   <tr>
+     <th scope="row">3</th>
+     <td >Larry the Bird</td>
+     <td>Pigeon</td>
+     <td>50</td>
+     <td>M</td>
+     <td>Menuisier</td>
+   </tr>
+ </tbody>
+</table>
+fred;
+echo($text);
+?>
+    
+</body>
+</html>
